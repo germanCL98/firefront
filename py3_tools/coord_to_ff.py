ca0a899d246f9add8de6521707786e36a91ca334
Cristobal Bergillos Navarro <cristobal.bergillosnavarro@avincis.eu>
2023-12-04 16:15:14 +0100

    Clean execution args

--- py3_tools/coord_to_ff.py
+++ py3_tools/coord_to_ff.py
@@ -1,38 +1,32 @@
-import argparse
+import click
+from datetime import datetime
 
 from forefirepy.ForeFire import *
 
-def main():
-	ap = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-	ap.add_argument(
-        "--lat",
-				default=41.6,
-        help="latitude of fire start"
+
+@click.command()
+@click.option("--lat", default=41.6, help="latitude of fire start")
+@click.option("--lon", default=9.2, help="longitude of fire start")
+@click.option("--crsin", default=4326, help="Input EPSG Code")
+@click.option("--crsout", default=32632, help="Output EPSG Code")
+@click.argument("output_file", type=click.Path())
+def main(lat, lon, crsin, crsout, output_file):
+    [x, y] = reproject(
+        [lon, lat],
+        inEpsg="epsg:" + crsin,
+        outEpsg="epsg:" + crsout,
     )
-	ap.add_argument(
-        "--lon",
-				default=9.2,
-        help="longitude of fire start"
-		)
-	args = vars(ap.parse_args())
-	lat = args.get('lat')
-	lon = args.get('lon')
 
-	dir_path = os.path.dirname(os.path.realpath(__file__))
-	output_path = dir_path + '/../examples/aullene/'
-	filename = f'aullene2.ff'
-	complete_path = output_path + filename
+    ff = Forefire()
+    ff.configBasicFf(lon=x, lat=y)
+    ff.saveFf(output_file)
 
-	[x, y] = reproject([lon, lat], inEpsg='epsg:4326', outEpsg='epsg:32632')
+    # os.system(f"cd {output_path}; forefire -i {filename}")
+    os.system(f"forefire -i {output_file}")
 
-	ff = Forefire()
-	ff.configBasicFf(lon=x, lat=y)
-	ff.saveFf(complete_path)
-
-	os.system(f'cd {output_path}; forefire -i {filename}')
-	
-	ff.convert_to_geojson(output_path + '0-2009-07-24T14-57-39Z.ffgeojson')
+    # ff.convert_to_geojson(output_path + "0-2009-07-24T14-57-39Z.ffgeojson")
+    ff.convert_to_geojson("0-2009-07-24T14-57-39Z.ffgeojson")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
